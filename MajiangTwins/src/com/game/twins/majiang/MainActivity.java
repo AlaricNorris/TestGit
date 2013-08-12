e8cd3dfee35f909bf5ba3efe35023eb6996841db
Norris <Norris@Norris-OC>
2013-08-12 09:43:53 +0800

    sdfasdf

--- MajiangTwins/src/com/game/twins/majiang/MainActivity.java
+++ MajiangTwins/src/com/game/twins/majiang/MainActivity.java
@@ -1,13 +1,17 @@
 package com.game.twins.majiang;
 
-import android.app.Activity ;
-import android.os.Bundle ;
+import android.app.Activity;
+import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.act_main);
-    }
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.act_main );
+
+		// °¢Ë¹µÙ·Ò
+	}
 
 }
